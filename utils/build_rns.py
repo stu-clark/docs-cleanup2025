2012130ecfce17833011a51f7c23f690494cad3d
Stu Clark <sclark@nvidia.com>
2025-10-08 11:55:57 -0400

    Add very old NetQ EoS to EOL_VERSIONS list in rn script. Add expired skel dir for EoS NetQ versions overlapping with CL versions.

--- utils/build_rns.py
+++ utils/build_rns.py
@@ -418,9 +418,9 @@ def build_rn_markdown_files(product, version_list):
 
     for version in version_list:
     #### Examples of skipping versions for RN processing - list of EOL_VERSIONS regardless of product, or individual version numbers:
-    # EOL_VERSIONS = [3.7.0]
-    # if version in EOL_VERSIONS:
-    #    continue
+        EOL_VERSIONS = [ "2.4.0", "2.4.1", "3.0.0", "3.1.0", "3.2.0", "3.2.1", "3.3.0", "3.3.1" ]
+        if version in EOL_VERSIONS:
+            continue
     #   if version == "4.6.0":
     #       continue
     #    if version == "4.12.0":
